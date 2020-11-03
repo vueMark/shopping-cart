f95deb8cf3565b113222a5c766048574acde1c07
mark.costales <mcostales@t2s-solutions.com>
2020-11-02 22:32:44 -0500

    access store from all components

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -12,7 +12,6 @@
 
 <script>
 
-import store from '@/store/index'
 export default {
 	data(){
 		return{
@@ -21,12 +20,12 @@ export default {
 	},
 	computed:{
 		products(){
-			return store.getters.availableProducts
+			return this.$store.getters.availableProducts
 		}
 	},
 	created(){
 		this.loading = true
-		store.dispatch('fetchProducts')
+		this.$store.dispatch('fetchProducts')
 		.then(()=>this.loading=false)
 	}
 }
